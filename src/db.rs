1a04685ad9c24d0b75e8c870126b7aebd5e0c676
cristan <13849765+cristan2@users.noreply.github.com>
2020-11-20 23:56:37 +0200

    Adaugat query editii

--- src/db.rs
+++ src/db.rs
@@ -1,33 +1,87 @@
 use rusqlite::{Connection, Result, Statement};
 use rusqlite::NO_PARAMS;
 
+// TODO generic API
+// - error handling
+// - parse row for Option<T>: null or empty string -> None
+// - parse row for T (NOT NULL): empty string -> ???
+
 #[derive(Debug)]
 pub struct Revista {
     id: i32,
     nume: String,
-    alias: String,
-    status: String,
-    tip: String,
-    perioada: String,
-    aparitii: String,
-    descriere: String,
-    link: String,
-    observatii: String,
+    alias: Option<String>,
+    status: Option<String>,
+    tip: Option<String>,
+    perioada: Option<String>,
+    aparitii: Option<String>,
+    descriere: Option<String>,
+    link: Option<String>,
+    observatii: Option<String>,
 }
 
 impl Revista {
     pub fn from_row(row: &rusqlite::Row) -> Revista {
         Revista {
             id: row.get("revista_id").unwrap(),
-            nume: row.get("revista_nume").unwrap_or("".to_string()),
-            alias: row.get("revista_alias").unwrap_or("".to_string()),
-            status: row.get("status").unwrap_or("".to_string()),
-            tip: row.get("tip").unwrap_or("".to_string()),
-            perioada: row.get("perioada").unwrap_or("".to_string()),
-            aparitii: row.get("aparitii").unwrap_or("".to_string()),
-            descriere: row.get("descriere").unwrap_or("".to_string()),
-            link: row.get("link_oficial").unwrap_or("".to_string()),
-            observatii: row.get("observatii").unwrap_or("".to_string()),
+            nume: row.get("revista_nume").unwrap(),
+            alias: row.get("revista_alias").ok(),
+            status: row.get("status").ok(),
+            tip: row.get("tip").ok(),
+            perioada: row.get("perioada").ok(),
+            aparitii: row.get("aparitii").ok(),
+            descriere: row.get("descriere").ok(),
+            link: row.get("link_oficial").ok(),
+            observatii: row.get("observatii").ok(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Editie {
+    editie_id: i32,
+    revista_id: i32,
+    tip: Option<String>,
+    parinte_editie_id: Option<String>,
+    numar: i32,
+    an: i32,
+    luna: i32,
+    luna_sfarsit: Option<String>,
+    pret: Option<i32>,
+    nr_pagini: Option<i32>,
+    disc_demo: Option<String>,
+    joc_complet: Option<String>,
+    redactor_sef: Option<String>,
+    editie_link_oficial: Option<String>,
+    editie_observatii: Option<String>,
+    scan_info_nr_pg: Option<i32>,
+    scan_info_pg_lipsa: Option<String>,
+    scan_info_observatii: Option<String>,
+    scan_info_credits: Option<String>
+}
+
+impl Editie {
+    pub fn from_row(row: &rusqlite::Row) -> Editie {
+        Editie {
+            editie_id: row.get("editie_id").unwrap(),
+            revista_id: row.get("revista_id").unwrap(),
+            tip: row.get("tip").unwrap(),
+            parinte_editie_id: row.get("parinte_editie_id").unwrap(),
+            numar: row.get("numar").unwrap_or(99),  // error ???
+            an: row.get("an").unwrap(),
+            luna: row.get("luna").unwrap_or(99),  // de inlocuit
+            luna_sfarsit: row.get("luna_sfarsit").ok(),
+            pret: row.get("pret").ok(),   // in DB e NUMERIC  // error
+            nr_pagini: row.get("nr_pagini").ok(),
+            disc_demo: row.get("disc_demo").ok(),
+            joc_complet: row.get("joc_complet").ok(),
+            redactor_sef: row.get("redactor_sef").ok(),
+            editie_link_oficial: row.get("editie_link_oficial").ok(),
+            editie_observatii: row.get("editie_observatii").ok(),
+            scan_info_nr_pg: row.get("scan_info_nr_pg").ok(),
+            scan_info_pg_lipsa: row.get("scan_info_pg_lipsa").ok(),
+            scan_info_observatii: row.get("scan_info_observatii").ok(),
+            scan_info_credits: row.get("scan_info_credits").ok(),
         }
     }
 }
@@ -50,6 +104,9 @@ impl DBConnection {
     }
 
     // TODO move queries to API trait
+
+    /* --- Reviste --- */
+
     pub fn retrieve_toate_revistele(&self) -> Vec<Result<Revista>> {
         let mut stmt_reviste: Statement = self.prepare_statement(
             "
@@ -75,5 +132,18 @@ impl DBConnection {
             .unwrap();
 
         revista
+    }
+
+    /* --- Editii --- */
+    pub fn retrieve_toate_editiile(&self) -> Vec<Result<Editie>> {
+        let mut stmt_editii: Statement = self.prepare_statement(
+            "
+            SELECT * FROM editii
+            ");
+
+        stmt_editii
+            .query_map(NO_PARAMS, |row| Ok(Editie::from_row(row)))
+            .unwrap()
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,25 @@ use arhiva_reviste_ruginite::db::*;
 
 fn main() -> Result<()> {
 
-    let conn = DBConnection::open("arhiva_reviste_v7.0.db");
+    let db = DBConnection::open("arhiva_reviste_v7.0.db");
 
-    let reviste = conn.retrieve_toate_revistele();
+    let reviste = db.retrieve_toate_revistele();
 
     let rev_id = 1;
-    let revista: Result<Revista> = conn.retrieve_revista(&rev_id);
+    let revista: Result<Revista> = db.retrieve_revista(&rev_id);
 
-    for revista in reviste {
-        println!("{:?}", revista.unwrap());
+    for rev in reviste {
+        println!("{:?}", rev.unwrap());
     }
 
     println!("{:?}", revista);
 
+    let editii = db.retrieve_toate_editiile();
+
+
+    for editie in editii {
+        println!("{:?}", editie.unwrap());
+    }
 
     Ok(())
 }
